b372d93da3b586150b5d6819b08f710f510446d9
Maxim Akkuzin <akkmaxon2307@gmail.com>
2015-12-14 19:06:52 +0300

    Random articles on index page

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,11 @@
 module ArticlesHelper
+  def random_articles
+    articles = current_user.articles
+    random_articles = []
+    articles_on_page = 3
+    articles_on_page.times do
+      random_articles << articles.limit(1).offset(rand(articles.count)).first
+    end
+    random_articles.uniq
+  end
 end
