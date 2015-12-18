8aec27459f1ca4732cb421df82d0e68597389f52
Maxim Akkuzin <akkmaxon2307@gmail.com>
2015-12-18 23:20:13 +0300

    Changed title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def proper_search_place
     params[:action] == 'index' || params[:action] == 'show'
   end
+
+  def full_title(app_title)
+    if current_user
+      current_user.name + " | " + app_title 
+    else
+      app_title
+    end
+  end
 end
