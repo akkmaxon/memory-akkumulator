e36a86ee33f9daacaa4b77abd89d2131cfb44357
Maxim Akkuzin <akkmaxon2307@gmail.com>
2016-01-25 12:59:57 +0300

    Style welcome page, buttons

--- app/controllers/welcome_to_app_controller.rb
+++ app/controllers/welcome_to_app_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeToAppController < ApplicationController
   def welcome
+    if signed_in?
+      redirect_to articles_path
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,2 @@
 module ApplicationHelper
-  def proper_search_place
-    actions = %q{ index show search }
-    actions.include? params[:action]
-  end
-
-  def full_title(app_title)
-    if current_user
-      app_title + " | " + current_user.email
-    else
-      app_title
-    end
-  end
 end
